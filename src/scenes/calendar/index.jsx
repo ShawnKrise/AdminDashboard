fea69e5bcf23bb4c4a49a5ebef70596ca3c8ce92
Shawn <shawnkrise@gmail.com>
2023-03-16 15:28:58 -0600

    Fixed Calendar section

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,42 +1,45 @@
 import { useState } from "react";
-import FullCalendar, { formatDate } from "@FullCalendar/react";
+import FullCalendar from "@fullcalendar/react";
+import { formatDate } from '@fullcalendar/core';
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
-import {
-    Box,
-    List,
-    ListItem,
-    ListItemText,
-    Typography,
-    useTheme,
-  } from "@mui/material";
 
-  const Calendar = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode)
-    const [currentEvents, setCurrentEvents] = useState([])
+//importing FullCalendar for Calendar dependancy
+const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
 
-    const handleDateClick = (selected) => {
-        const title = prompt("Please enter a new title for your next event");
-        const calendarAPI = selected.view.calendar;
-        calendarAPI.unselect();
+  //create a const for date click
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter a new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
 
-        if (title) {
-            calendarAPI.addEvent({
-                id: `${selected.dataStr}-${title}`,
-                title,
-                start: selected.startStr,
-                end: selected.endStr,
-                allDay: selected.allDay
-            });
-        };
-    };
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
   };
 
+  //create a const for handle event click
   const handleEventClick = (selected) => {
     if (
       window.confirm(
@@ -47,5 +50,87 @@ import {
     }
   };
 
-  
-  export default Calendar;
+  return (
+    <Box m="20px">
+      <Header title="Calendar" subtitle="Full Calendar Page" />
+
+      <Box display="flex" justifyContent="space-between">
+        {/* Calendar sidebar */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5"> Scheduled Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        {/* Calendar */}
+        {/* Note: included ID's automatically displayed on calendar for initialevent demonstration purposes. */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "12315",
+                title: "All-day event",
+                date: "2023-03-16",
+              },
+              {
+                id: "5123",
+                title: "Timed event",
+                date: "2023-03-17",
+              },
+            ]}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Calendar;
