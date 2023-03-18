8e5d9bb195d3df3a844a3ca654ca9da01a8cf0e4
Shawn <shawnkrise@gmail.com>
2023-03-17 19:24:03 -0600

    added them to geo

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,8 +1,13 @@
 import { Box, colors } from "@mui/material";
 import Header from "../../components/Header";
 import GeographyChart from "../../components/GeographyChart";
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
 
 const Geography = () => {
+    const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
     return (
         <Box m="20px">
             <Header title="Geography Chart" subtitle="Simple Geography Chart" />
